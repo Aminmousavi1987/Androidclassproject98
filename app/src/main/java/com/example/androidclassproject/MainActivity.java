393b70da48d7788f207c29efc45891511a49e424
Amin <black.eagle52@yahoo.com>
2019-10-16 02:55:08 -0700

    firstactivity

--- app/src/main/java/com/example/androidclassproject/MainActivity.java
+++ app/src/main/java/com/example/androidclassproject/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
  public class MainActivity extends AppCompatActivity {
 
@@ -13,35 +14,10 @@ import android.util.Log;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MYTAG","in oncreate");
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("MYTAG","in on start");
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("MYTAG","in onresume");
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("MYTAG","in onpause");
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("MYTAG","in on stop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("MYTAG","in ondestroy");
-    }
 }
